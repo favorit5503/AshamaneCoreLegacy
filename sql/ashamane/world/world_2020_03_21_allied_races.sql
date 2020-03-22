77a21af1cf2a6fa071c704798192ce4150a10e74
Varjgard <61460712+Varjgard@users.noreply.github.com>
2020-03-22 08:14:42 +0200

    enable allied races, fix death knights "nothing to dispell error", some startup fixes.
    -death knights "nothing to dispell" error fixed.
    -enabled allied races.
    -fixed some startup errors.

---
+++ sql/ashamane/world/world_2020_03_21_allied_races.sql
@@ -0,0 +1,15 @@
+/*
+AshamaneCoreLegacy-Legion
+cb255034ad1c6438ab2d5618ec09c329765b9d4e
+2020-03-17
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+
+replace into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('27','6','0');
+replace into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('28','6','0');
+replace into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('29','6','0');
+replace into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('30','6','0');
+
