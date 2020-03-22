32a12c2e61d589d26ba9413b4c49da3d9d4d9949
Varjgard <61460712+Varjgard@users.noreply.github.com>
2020-03-22 09:38:06 +0200

    update sql syntax

--- sql/ashamane/world/world_2020_03_21_allied_races.sql
+++ sql/ashamane/world/world_2020_03_21_allied_races.sql
@@ -7,9 +7,13 @@ cb255034ad1c6438ab2d5618ec09c329765b9d4e
 
 /*!40101 SET NAMES utf8 */;
 
+DELETE FROM `race_unlock_requirement` WHERE `raceID`=27;
+DELETE FROM `race_unlock_requirement` WHERE `raceID`=28;
+DELETE FROM `race_unlock_requirement` WHERE `raceID`=29;
+DELETE FROM `race_unlock_requirement` WHERE `raceID`=30;
 
-replace into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('27','6','0');
-replace into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('28','6','0');
-replace into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('29','6','0');
-replace into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('30','6','0');
+insert into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('27','6','0');
+insert into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('28','6','0');
+insert into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('29','6','0');
+insert into `race_unlock_requirement` (`raceID`, `expansion`, `achievementId`) values('30','6','0');
 
